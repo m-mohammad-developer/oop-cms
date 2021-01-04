114d48530545e0243d666fcf8ba3d0e145c47a06
Mr.Programmer <m.mohammadi.developer@gmail.com>
2021-01-04 12:06:08 -0800

    add-some-features

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -4,8 +4,6 @@ session_start();
 // Define directory separator
 defined("DS") ? : define("DS", DIRECTORY_SEPARATOR);
 // Define site root
-//C:\xampp\htdocs\oop-cms
-// defined("SITE_ROOT") ? : define("SITE_ROOT", "C:" .DS. "xampp" .DS. 'htdocs' .DS. "oop-cms");
 defined("SITE_ROOT") ? : define("SITE_ROOT", "C:" .DS. "xampp" .DS. 'htdocs' .DS. "oop-cms");
 // Define includes path
 defined("INCLUDES_PATH") ? : define("INCLUDES_PATH", SITE_ROOT.DS."admin".DS."includes");
@@ -16,9 +14,7 @@ defined("HOST_NAME") ? : define("HOST_NAME", 'localhost');
 // define upload directory
 defined("UPLOAD_DIR") ? : define("UPLOAD_DIR", SITE_ROOT.DS."uploads");
 
-
-
-
+//
 require_once INCLUDES_PATH . DS . 'functions.php';
 
 require_once 'vendor/autoload.php';
